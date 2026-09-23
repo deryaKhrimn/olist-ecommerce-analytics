8f0264fc1ca3056ed2b57d308002c330cfc42d53
deryaKhrimn <deryakahriman337@gmail.com>
2026-09-23 13:04:35 +0300

    feat: Kaggle API entegrasyonu sağlandı ve ilk EDA yapıldı

---
+++ scripts/extract_load.py
@@ -0,0 +1,16 @@
+# scripts/extract_load.py
+import kaggle
+import os
+import zipfile
+
+def download_olist_data():
+    dataset = "olistbr/brazilian-ecommerce"
+    download_path = "data/raw"
+    
+    print(f"Kaggle'dan {dataset} veri seti indiriliyor...")
+    # Kaggle API kullanarak veriyi indirme
+    kaggle.api.dataset_download_files(dataset, path=download_path, unzip=True)
+    print("Veri indirme ve zipten çıkarma işlemi tamamlandı!")
+
+if __name__ == "__main__":
+    download_olist_data()
